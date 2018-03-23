4b376727362924ebf43acd5d5dce86ebd8ff0412
ariska <ariska138@gmail.com>
2018-03-23 17:12:34 +0700

    add project latihan6

---
+++ Assets/Latihan6.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan6 : MonoBehaviour {
+
+	public int[] intArray; // Deklarasi Array
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Menampilkan Seluruh Array");
+		foreach (int a in intArray) {
+			Debug.Log (a);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
