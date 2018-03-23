a76f9dc94af22dc000a7a1ef56398aeddd23998f
ariska <ariska138@gmail.com>
2018-03-23 17:02:49 +0700

    add project latihan4

---
+++ Assets/Latihan4/Karakter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Karakter {
+	private string name;
+
+	private int health;
+
+	private int damage;
+
+	public string Name{
+		get{
+			return name;
+		}set{
+			name = value;
+		} 
+	}
+
+	public int Health{
+		get{
+			return health;
+		}set{
+			health = value;
+		} 
+	}
+
+	public int Damage{
+		get{
+			return damage;
+		}set{
+			damage = value;
+		} 
+	}
+
+	public void MoveLeft(){
+		Debug.Log ("Gerak ke kiri");
+	}
+
+	public void MoveRight(){
+		Debug.Log ("Gerak ke kekanan");
+	}
+
+	public void Jump(){
+		Debug.Log ("Loncat");
+	}
+
+	public void Attack(){
+		Debug.Log ("Serang");
+	}
+
+
+}
---
+++ Assets/Latihan4/Latihan4.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan4 : MonoBehaviour {
+
+	Karakter player1;
+
+	// Use this for initialization
+	void Start () {
+		player1 = new Karakter ();
+		player1.Name = "Dico";
+		player1.Health = 100;
+		player1.Damage = 30;
+		//Menampilkan hasil dari inisialisasi attribute
+		Debug.Log ("Name: "+player1.Name+", Health: "+player1.Health+", Damage: "+player1.Damage);
+		//Memanggil method dari salah satu method di class Karakter.
+		player1.Jump ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
