42863474a0dfca9c75f0ed2033a2eaf87f4449a4
ariska <ariska138@gmail.com>
2018-03-23 16:18:35 +0700

    project latihan 2

---
+++ Assets/Latihan2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Latihan2 : MonoBehaviour {
+
+	public int nilaiA;
+	public int nilaiB;
+	[Range(1,5)]
+	public int nilaiC;
+	// Use this for initialization
+	void Start () {
+		int totall = (nilaiA + nilaiB) * nilaiC;
+		Debug.Log("Total "+totall); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
