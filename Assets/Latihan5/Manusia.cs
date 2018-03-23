aa8792d24dc788861e728ea6397303eef34aa1e5
ariska <ariska138@gmail.com>
2018-03-23 17:05:16 +0700

    add project latihan5

---
+++ Assets/Latihan5/Manusia.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manusia: MonoBehaviour{
+
+	public void Makan(){
+		Debug.Log ("Perlu Makan");
+	}
+
+	public void Tidur(){
+		Debug.Log ("Perlu Tidur");
+	}
+}
---
+++ Assets/Latihan5/Prajurit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prajurit : Manusia {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Seorang Prajurit dapat : ");
+		Makan ();
+		Tidur ();
+		Menyerang ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Menyerang(){
+		Debug.Log ("Menyerang");
+	}
+}
